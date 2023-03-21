e2ffe261a621706e0d6e76797dd701d586390c15
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2023-03-21 13:09:00 +0000

    cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,14 @@
 import Fastify from "fastify";
-import {PrismaClient} from "@prisma/client"
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
 
 const app = Fastify();
 const prisma = new PrismaClient();
 
+app.register(cors)
+
 app.get("/hello", async () => {
-  const habits = await prisma.habit.findMany({
-    where: {
-      title: {
-        startsWith: 'd'
-      }
-    }
-  });
+  const habits = await prisma.habit.findMany();
 
   return habits;
 });
