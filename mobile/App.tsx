cae8dc751f351a1c133e298766cbea0e4249db15
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2023-03-22 18:26:39 +0000

    fontsLoaded

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -10,13 +10,17 @@ import {
 import { Loading } from "./src/components/Loading";
 
 export default function App() {
-    const [fontsLoaded] = useFonts({
-      Inter_400Regular,
-      Inter_600SemiBold,
-      Inter_700Bold,
-      Inter_800ExtraBold,
-    });
-  
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>
