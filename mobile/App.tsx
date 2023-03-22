ec9eb9b03e76d0d6bb06fcffe8849298197a13cb
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2023-03-22 18:20:36 +0000

    initial styles

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -13,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#09090A",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#FFF",
+    fontFamily: "Inter_800ExtraBold",
   },
 });
