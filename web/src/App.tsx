6e1d13c69231fb2ad8e9b724102736436cc12bd2
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2023-03-28 09:33:10 +0100

    App.tsx basic structure

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,14 @@
-import './styles/global.css'
+import "./styles/global.css";
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
 
-function App() {
+export function App() {
   return (
-    <div className="bg-red-500">30</div>
-  )
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
+  );
 }
-
-export default App
